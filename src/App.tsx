a73fdebc378b0ce264cbfe0b2fcd5ed00a693e0e
Ezewi Osorachukwu <oezewi@gmail.com>
2024-10-10 12:40:55 +0100

    First commit for miranda react tutorial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Button from './components/Button'
+
+
+function App() {
+  
+  return (
+    <div>
+      <h1 style={{color: "blue"}}> React Tutorial </h1>
+
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,6 @@
+function Button() {
+  return (
+    <button style={{backgroundColor: "pink"}}>Click</button>
+  )
+}
+export default Button;
